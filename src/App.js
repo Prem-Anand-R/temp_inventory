610133f9269a3f971a11a95b41ecdf71ec42b610
Prem-Anand-R <premanand484@gmail.com>
2024-10-20 01:44:02 -0700

    Updated package files and App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import LoginPage from './Components/Pages/LoginPage';
-import { BrowserRouter, Route, Routes,Navigate} from 'react-router-dom';
+// import { BrowserRouter, Route, Routes,Navigate} from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './Components/Pages/Dashboard'
 import Sidebar from './Components/Sidebar';
 import Product_List from './Components/Pages/Product_List';
@@ -23,35 +24,61 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+   
+
+    <HashRouter>
       <Routes>
-	  
-        <Route path="/" element={!loggedIn ? (<LoginPage  setLoggedIn={handlelogin} />):(<Navigate to={'/dashboard'} />)} />
-        <Route path="/dashboard" element={loggedIn ? (<Sidebar ><Dashboard setLoggedIn={handlelogout}/> </Sidebar>) :(<Navigate to={"/"} />)} />
-          <Route path="/outward" element={loggedIn ? (
-          <Sidebar>
-            <Outward />
-          </Sidebar>):(<Navigate to={"/"} />)
-
-          }/>
-          <Route path="/inward" element={
-           loggedIn ? (
+        <Route
+          path="/"
+          element={!loggedIn ? (
+            <LoginPage setLoggedIn={handlelogin} />
+          ) : (
+            <Navigate to="/dashboard" />
+          )}
+        />
+        <Route
+          path="/dashboard"
+          element={loggedIn ? (
             <Sidebar>
-              <Inward/>
-            </Sidebar>):(<Navigate to={"/"} />)
-
-          }/>
-          <Route path="/productList" element={
-            loggedIn ? (
-            <Sidebar >
+              <Dashboard setLoggedIn={handlelogout} />
+            </Sidebar>
+          ) : (
+            <Navigate to="/" />
+          )}
+        />
+        <Route
+          path="/outward"
+          element={loggedIn ? (
+            <Sidebar>
+              <Outward />
+            </Sidebar>
+          ) : (
+            <Navigate to="/" />
+          )}
+        />
+        <Route
+          path="/inward"
+          element={loggedIn ? (
+            <Sidebar>
+              <Inward />
+            </Sidebar>
+          ) : (
+            <Navigate to="/" />
+          )}
+        />
+        <Route
+          path="/productList"
+          element={loggedIn ? (
+            <Sidebar>
               <Product_List />
-            </Sidebar>):(<Navigate to={"/"} />)
-
-          }/>
-         </Routes>
-
-
-    </BrowserRouter>
+            </Sidebar>
+          ) : (
+            <Navigate to="/" />
+          )}
+        />
+      </Routes>
+    </HashRouter>
+    
 
   );
 }
